2b30ea58c6edcd5e44d38ef2e6c764251894976f
Ed Bull <eddybull@hotmail.com>
2023-09-20 00:50:22 +0700

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,15 @@
+
+import os
+os.system('clear')
+
+
+first_name = 'Eddy'
+age = 45
+fav_pizza = {
+	"Eddy":"Pepperoni",
+        "Bob":"Mushrooms",
+        "Mary":"Cheese"
+        }
+name =True
+
+print(name) 
